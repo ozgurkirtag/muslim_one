6969f59ae4e2f6da83be3a296332ea5f9acc232f
ozgurkirtag <ozgurkirtag@gmail.com>
2026-07-20 07:10:39 +0000

    Add AdMob SDK using Ten Sec Rush setup

---
+++ lib/widgets/banner_ad_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdWidget extends StatefulWidget {
+  const BannerAdWidget({super.key});
+
+  @override
+  State<BannerAdWidget> createState() => _BannerAdWidgetState();
+}
+
+class _BannerAdWidgetState extends State<BannerAdWidget> {
+  static const String _testBannerId =
+      'ca-app-pub-3940256099942544/6300978111';
+
+  BannerAd? _bannerAd;
+  bool _loaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (!kIsWeb) {
+      _bannerAd = BannerAd(
+        adUnitId: _testBannerId,
+        size: AdSize.banner,
+        request: const AdRequest(),
+        listener: BannerAdListener(
+          onAdLoaded: (_) {
+            if (mounted) {
+              setState(() => _loaded = true);
+            }
+          },
+          onAdFailedToLoad: (ad, error) {
+            ad.dispose();
+            debugPrint('Banner yüklenemedi: $error');
+          },
+        ),
+      )..load();
+    }
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb || !_loaded || _bannerAd == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Container(
+      color: Colors.black,
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: _bannerAd!.size.height.toDouble(),
+      child: AdWidget(ad: _bannerAd!),
+    );
+  }
+}
